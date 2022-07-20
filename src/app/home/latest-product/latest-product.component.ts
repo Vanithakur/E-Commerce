00584297e9620492a614d8c383949b6198eceb3c
Vanithakur <st5930123@gmail.com>
2022-07-20 14:57:18 +0530

    shivani

--- src/app/home/latest-product/latest-product.component.ts
+++ src/app/home/latest-product/latest-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-latest-product',
   templateUrl: './latest-product.component.html',
@@ -10,6 +10,33 @@ export class LatestProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 300,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      },
+      
+      
+
+    },
+    nav: true
   }
 
 }
